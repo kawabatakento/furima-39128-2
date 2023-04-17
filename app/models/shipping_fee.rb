a30363850bca80bac1431878035bc2b727803df2
kawabatakento <is311806323@gmail.com>
2023-04-17 16:21:58 +0900

    出品情報入力項目

--- app/models/shipping_fee.rb
+++ app/models/shipping_fee.rb
@@ -1,5 +1,5 @@
 # app/models/shipping_fee_payer.rb
-class ShippingFeePayer < ActiveHash::Base
+class ShippingFee < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '着払い(購入者負担)' },
