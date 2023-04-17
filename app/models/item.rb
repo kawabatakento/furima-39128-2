01d92c6a626826203c20a399b242aceeae06b696
kawabatakento <is311806323@gmail.com>
2023-04-17 15:23:37 +0900

    アクテブハッシュとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_fee
+  belongs_to :shipping_time
+  end
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_time_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
 end
